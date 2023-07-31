b9e7b9ad8d0283b307203d8d34b6542a4810c427
NPyzhyanov <pyzhyanov_n@aol.com>
2023-07-31 19:42:14 +0300

    bugs fixed

--- OOP_11_02_library/leaver.h
+++ OOP_11_02_library/leaver.h
@@ -1,12 +1,10 @@
 #ifndef LEAVER_H
 #define LEAVER_H
 
-//#include "oop_11_02_library_global.h"
-
-#ifndef OOP_11_02_LIBRARY_EXPORTS //OOP_11_02_LIBRARY_LIBRARY//OOP_11_02_LIBRARY_EXPORTS
-#   define OOP_11_02_LIBRARY_API __declspec(dllexport)
+#if defined OOP_11_02_LIBRARY_EXPORTS
+#define OOP_11_02_LIBRARY_API __declspec(dllexport)
 #else
-#   define OOP_11_02_LIBRARY_API __declspec(dllimport)
+#define OOP_11_02_LIBRARY_API __declspec(dllimport)
 #endif // DYNAMICLIB_EXPORTS
 
 class Leaver
